e53a4d8c379cffa669385d6892d3a326c4430417
pannavitsungthong-crypto <pannavit.sungthong@gmail.com>
2025-11-29 16:37:51 +0700

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -4,7 +4,7 @@ local KeyLink = "https://your-key-link.com" -- When pressing GET KEY, this link 
 
 --// LOADSTRING TO RUN AFTER CORRECT KEY
 local ScriptToRun = [[
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/baconluck7-stack/ui-ex/refs/heads/main/ui%20ex"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/pannavitsungthong-crypto/Key/refs/heads/main/fps%20boot.lua"))()
 
 ]]
 --// GUI CREATION
